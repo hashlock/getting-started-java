872e2070d5f76d9160acce6a3ae1cea46c724de6
hashlock <hans.ashlock@gmail.com>
2018-09-04 13:38:20 -0700

    test

--- helloworld-springboot/src/main/java/com/example/java/gettingstarted/HelloworldApplication.java
+++ helloworld-springboot/src/main/java/com/example/java/gettingstarted/HelloworldApplication.java
@@ -26,7 +26,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloworldApplication {
   @RequestMapping("/")
   public String home() {
-    return "Hello World jFrog!";
+    return "Hello World Test!";
   }
 
   /**
